a722ea98d9ef37aa058080cd8d66ae96ff472ea6
CristinaJung <cristijung@gmail.com>
2025-05-13 16:28:55 -0300

    inicio da estrutura

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
 import React from "react";
-import Header from "@/components/header/Header";
-import Footer from "@/components/footer/Footer";
+
 
 export default function Home() {
   return (
-    <>
-      <Header/>
-      <h1>Conteúdo</h1>
-      <Footer/>
+    <>  
+       Esta é a tela inicial da minha aplicação      
     </>
   );
 }
--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,11 +1,16 @@
+import Link from "next/link";
 import React from "react";
+import styles from './navbar.module.scss';
 
 export default function NavBar() {
     return(
         <>
-        <nav>
-
-            
+        <nav className={styles.navLink}>
+            <Link href='/home'>Home</Link>
+            <a href="/home">Home</a>
+            <Link href='/quemSomos'>Quem Somos</Link>
+            <Link href='/portfolio'>Portfólio</Link>
+            <Link href='/contato'>Contato</Link>            
         </nav>
         </>
     );
