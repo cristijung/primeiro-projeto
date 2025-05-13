32196ab227aa742cc31eceb81a46b9d2c169c3d0
CristinaJung <cristijung@gmail.com>
2025-05-13 15:26:15 -0300

    inicial

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# primeiro-projeto
+# Primeiro Projeto React/NextJs
+
+## Instalações e Versões
+
+- Sass (scss): `npm i sass --save`
---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <>
+      <div>
+        <h1>Olá</h1>
+      </div>
+      <div>
+        <h2>Oi</h2>
+      </div>
+    </>
+  );
+}
