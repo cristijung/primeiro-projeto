478a6b80b42be16d54505354cb780b1e0e517656
CristinaJung <cristijung@gmail.com>
2025-05-19 13:56:14 -0300

    algo

---
+++ src/app/layoutWrapper.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import Header from "@/components/header/Header";
+import Footer from "@/components/footer/Footer";
+
+export const metadata: Metadata = {
+  title: "Primeiro Projeto React/NextJs",
+  description: "Mini projeto usado para explicação em aula",
+};
+
+export default function LayoutWrapper({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <Header/>
+        {children}
+        <Footer/>
+      </body>
+    </html>
+  );
+}
